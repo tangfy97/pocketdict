4f64602dcf3f0c93892698627950536964ec823c
Feiyang Tang <f.tang@sig.eu>
2024-06-10 14:20:58 +0200

    Initial Commit

---
+++ pocketdict/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pocketdict
+//
+//  Created by Feiyang Tang on 10/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
